e9247338d44fbc1a5551e3a29f373ab3d9e2601e
manami-sato <manamisato2001@gmail.com>
2022-07-08 11:35:19 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  head: {
+    meta: [
+      {
+        name: "description",
+        content: "月波の写真ポートフォリオサイトです。",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1.0,minimum-scale=1.0",
+      },
+      { charset: "utf-8" },
+      { property: "og:type", content: "website" },
+      { property: "og:site_name", content: "Philosophia" },
+      { property: "og:title", content: "Philosophia" }, //- サイト自体のタイトル
+      {
+        property: "og:description",
+        content: "月波の写真ポートフォリオサイトです。",
+      }, //- サイトの説明
+      {
+        property: "og:url",
+        content: "https://click.ecc.ac.jp/ecc/msatou/Philosophia/",
+      }, //- サイトのURL
+      {
+        property: "og:image",
+        content: "https://manami-sato.github.io/philosophia/img/img_32_06.jpg",
+      }, //- サイトのカバー画像
+      { rel: "apple-touch-icon", href: "@/assets/img/favicon.png" },
+    ],
+  },
+};
+</script>
---
+++ src/mixins/Mixin.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      path: "https://click.ecc.ac.jp/ecc/msatou/Philosophia/",
+      imgPath: "https://manami-sato.github.io/philosophia/",
+      productsData:
+        "https://click.ecc.ac.jp/ecc/msatou/products/philosophia.php",
+      name: "月波",
+      enName: "Tsukinami",
+      yearList: [
+        {
+          id: 2022,
+          flag: false,
+        },
+        {
+          id: 2021,
+          flag: false,
+        },
+        {
+          id: 2020,
+          flag: false,
+        },
+        {
+          id: 2019,
+          flag: false,
+        },
+        {
+          id: 2018,
+          flag: false,
+        },
+      ],
+      height: 0,
+    };
+  },
+  mounted() {
+    this.height = window.innerHeight;
+  },
+};
+</script>
